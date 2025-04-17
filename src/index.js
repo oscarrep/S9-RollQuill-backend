e8f45f72ef0646aff0dcc906d823911ab4ef9f66
oscarrep <oscar.bcn.95@gmail.com>
2025-04-17 14:37:46 +0200

    set up routing and controllers

--- src/index.js
+++ src/index.js
@@ -1,69 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Character = require('./models/character.model.js')
+const Character = require('./models/character.model.js');
+const characterRoute = require('./routes/character.route.js');
 
 
 const app = express()
 app.use(express.json());
+
+app.use('/api/characters', characterRoute)
 
 dotenv.config();
 
 app.get('/', (request, response) => {
     response.send('Respone from node API');
 });
-
-app.post('/api/characters', async (request, response) => {
-    try {
-        const character = await Character.create(request.body);
-        response.status(200).json(character);
-    }
-    catch (error) { response.status(500).json({ message: error.message }); }
-});
-
-app.get('/api/characters', async (request, response) => {
-    try {
-        const characters = await Character.find({});
-        response.status(200).json(characters);
-    }
-    catch (error) { response.status(500).json({ message: error.message }); }
-});
-
-app.get('/api/character/:id', async (request, response) => {
-    try {
-        const { id } = request.params
-        const character = await Character.findById(id);
-        response.status(200).json(character);
-    }
-    catch (error) { response.status(500).json({ message: error.message }); }
-});
-
-app.put('/api/character/:id', async (request, response) => {
-    try {
-        const { id } = request.params
-        const character = await Character.findByIdAndUpdate(id, request.body);
-
-        if (!character) return response.status(404).json({ message: "Character not found" });
-
-        const updatedCharacter = await Character.findById(id);
-        response.status(200).json(updatedCharacter);
-    }
-    catch (error) { response.status(500).json({ message: error.message }); }
-});
-
-app.delete('/api/character/:id', async (request, response) => {
-    try {
-        const { id } = request.params
-        const character = await Character.findByIdAndDelete(id);
-
-        if (!character) return response.status(404).json({ message: "Character not found" });
-        
-        response.status(200).json({message:`Deleted character with id: ${id}`});
-
-
-    }
-    catch (error) { response.status(500).json({ message: error.message }); }
-})
 
 mongoose.connect(`${process.env.CONNECT_STR}`)
     .then(() => {
