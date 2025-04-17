07ecaa21c56607f5505115e702829ac66c6797ef
oscarrep <oscar.bcn.95@gmail.com>
2025-04-17 02:43:26 +0200

    Connect mongoose + using .env

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import express from 'express'
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = express()
 
-app.listen(3000,()=>{
+dotenv.config();
+
+app.listen(3000, () => {
     console.log('Backend server running on port 3000');
 });
 
-app.get('/', (request, response)=>{
+app.get('/', (request, response) => {
     response.send('respone from node API');
+});
+
+mongoose.connect(`${process.env.CONNECT_STR}`)
+.then(()=>{
+    console.log("Database connected");
+})
+.catch(()=>{
+    console.log('Failed connection');
 })
