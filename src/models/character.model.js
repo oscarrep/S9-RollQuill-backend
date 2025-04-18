b899f16a2512ec379734ee1104737d9a71fc71ad
oscarrep <oscar.bcn.95@gmail.com>
2025-04-18 19:17:16 +0200

    Add user model

--- src/models/character.model.js
+++ src/models/character.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const characterSchema = mongoose.Schema(
     {
+        createdBy: {type: String},
         name: {type: String},
         race: {type: String},
         subrace: {type: String},
---
+++ src/models/user.model.js
@@ -0,0 +1,20 @@
+const userSchema = mongoose.Schema( 
+    {
+        fireUid: { type: String, required: true, unique: true },
+        username: {type: String, required: true }, 
+        premium: {type: String, default: false },
+        
+        races: {type: String}, 
+        subraces: {type: String}, 
+        classes: {type: String}, 
+        subclasses: {type: String}, 
+        spells: {type: Number}, 
+        items: {type: [Number]}, 
+        features: {type: [String]}, 
+
+    },
+{ timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
