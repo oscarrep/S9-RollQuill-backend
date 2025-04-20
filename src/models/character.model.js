b57e96911ef9280b5e71eab2ebe7aa1bcaa64c8d
oscarrep <oscar.bcn.95@gmail.com>
2025-04-20 11:42:32 +0200

    Add property names to stats array

--- src/models/character.model.js
+++ src/models/character.model.js
@@ -2,17 +2,24 @@ const mongoose = require('mongoose');
 
 const characterSchema = mongoose.Schema(
     {
-        createdBy: {type: String},
-        name: {type: String},
-        race: {type: String},
-        subrace: {type: String},
-        class: {type: String},
-        subclass: {type: String},
-        level: {type: Number},
-        speed: {type: Number},
-        stats: {type: [Number]},
-        skills: {type: [String]},
-        expertise: {type: [String]},
+        createdBy: { type: String },
+        name: { type: String, required: true },
+        race: { type: String, required: true },
+        subrace: { type: String, required: true },
+        class: { type: String, required: true },
+        subclass: { type: String, required: true },
+        level: { type: Number, required: true },
+        speed: { type: Number },
+        stats: {
+            strength: { type: Number, required: true },
+            dexterity: { type: Number, required: true },
+            constitution: { type: Number, required: true },
+            intelligence: { type: Number, required: true },
+            wisdom: { type: Number, required: true },
+            charisma: { type: Number, required: true },
+        },
+        skills: { type: [String], required: true },
+        expertise: { type: [String] },
     },
     { timestamps: true }
 );
