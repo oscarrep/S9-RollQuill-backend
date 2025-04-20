0429c7d635f0c3e46edb7f701183bb9c3a2cfbd0
oscarrep <oscar.bcn.95@gmail.com>
2025-04-20 03:50:04 +0200

    Add CORS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const Character = require('./models/character.model.js');
 const characterRoute = require('./routes/character.route.js');
 
 dotenv.config();
-
 const app = express()
+
+app.use(cors({
+    origin: (origin, callback) => {
+        const allowedOrigins = [
+            //'https://s9-rollquill.vercel.app',
+            'http://localhost:4200'
+        ];
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+}));
+
 app.use(express.json());
 
 app.use(process.env.API_CHARACTERS, characterRoute)
